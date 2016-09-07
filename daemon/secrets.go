40d498bf5cb3dd98adfffba7ee94bee4ebaf30da
Evan Hazlett <ejhazlett@gmail.com>
2016-09-07 19:18:48 -0400

    secrets: switch to tmpfs for secret volume
    Signed-off-by: Evan Hazlett <ejhazlett@gmail.com>

--- daemon/secrets.go
+++ daemon/secrets.go
@@ -1,8 +1,14 @@
 package daemon
 
 import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/cloudflare/cfssl/log"
 	"github.com/docker/docker/container"
 	"github.com/docker/docker/pkg/archive"
+	"github.com/docker/docker/pkg/mount"
 	"github.com/docker/docker/volume"
 )
 
@@ -10,6 +16,10 @@ const (
 	secretsVolumeName  = "secrets"
 	secretsVolumeLabel = "com.docker.secrets.container"
 )
+
+func secretsMountPath(c *container.Container) string {
+	return filepath.Join(c.Root, "secrets")
+}
 
 func (daemon *Daemon) setupSecrets(c *container.Container) error {
 	rdr, err := daemon.getSecrets(c)
@@ -20,33 +30,44 @@ func (daemon *Daemon) setupSecrets(c *container.Container) error {
 	m, ok := c.MountPoints[secretsContainerMountPath]
 	if !ok {
 		opts := map[string]string{
-			"type": "ramfs",
+			"type":   "tmpfs",
+			"device": "tmpfs",
 		}
 		labels := map[string]string{
 			secretsVolumeLabel: c.ID,
 		}
-		vol, err := daemon.VolumeCreate("", "", opts, labels)
+		vol, err := daemon.VolumeCreate("", "local", opts, labels)
 		if err != nil {
 			return err
 		}
+
+		logrus.Debugf("secrets: setting up secrets at %s", vol.Mountpoint)
+		if err := mount.Mount("tmpfs", vol.Mountpoint, "tmpfs", "nodev"); err != nil {
+			return fmt.Errorf("secrets: unable to setup mount: %s", err)
+		}
+
 		m = &volume.MountPoint{
 			Name:        vol.Name,
 			Source:      vol.Mountpoint,
 			Destination: secretsContainerMountPath,
 			RW:          false,
 		}
+		c.MountPoints[secretsContainerMountPath] = m
+
 		if c.Config.Labels == nil {
 			c.Config.Labels = map[string]string{}
 		}
 		c.Config.Labels[secretsVolumeLabel] = vol.Name
 	}
-	c.MountPoints[secretsVolumeName] = m
+
+	log.Debugf("secrets: mountpoint %+v", m)
 	if c.Config.Volumes == nil {
 		c.Config.Volumes = map[string]struct{}{}
 	}
 	c.Config.Volumes[m.Name] = struct{}{}
 
-	// TODO: populate volume
+	// populate volume
+	logrus.Debugf("secrets: populating volume data for %s -> %s", c.ID, m.Name)
 	if err := archive.Untar(rdr, m.Source, nil); err != nil {
 		return err
 	}
